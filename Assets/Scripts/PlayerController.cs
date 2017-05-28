d128e97471ee8b5a85cffc63307d344e00a9941b
Davide Comoretto <d.comoretto@tiscali.it>
2017-05-28 16:18:40 +0200

    Player controller, mover and placeholder

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent( typeof( PlayerMover ) )]
+public class PlayerController : MonoBehaviour
+{
+	private PlayerMover mover;
+
+	void Awake()
+	{
+		mover = GetComponent<PlayerMover>();
+	}
+
+	void Update()
+	{
+		if ( Input.GetButtonDown( "Jump" ) )
+		{
+			mover.Jump();
+		}
+
+		float hor = Input.GetAxisRaw( "Horizontal" );
+		mover.Walk( hor );
+	}
+}
---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[RequireComponent( typeof( Rigidbody2D ) )]
+public class PlayerMover : MonoBehaviour
+{
+	public float jumpingForce;
+	public float walkingSpeed;
+
+	private Rigidbody2D rbody;
+
+	void Awake()
+	{
+		rbody = GetComponent<Rigidbody2D>();
+	}
+
+	public void Jump()
+	{
+		rbody.AddForce( Vector2.up * jumpingForce, ForceMode2D.Impulse );
+	}
+
+	public void Walk( float offset )
+	{
+		Vector2 velocity = rbody.velocity;
+		velocity.x = offset * walkingSpeed;
+		rbody.velocity = velocity;
+	}
+}
