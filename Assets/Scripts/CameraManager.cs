25b423dac93a26911b7212d63168748a2b77a205
Davide Comoretto <d.comoretto@tiscali.it>
2017-05-28 14:52:45 +0200

    Main scene setup, camera movement

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+	private const float screenHeight = 16.875f;
+
+	public float minSpeed;
+	public float maxSpeed;
+	
+	void LateUpdate()
+	{
+		float averageY = 0;
+
+		if ( GameManager.instance.players.Count > 0 )
+		{
+			foreach ( GameObject player in GameManager.instance.players )
+			{
+				averageY += player.transform.position.y;
+			}
+
+			averageY /= GameManager.instance.players.Count;
+		}
+
+		float speedFactor = ((averageY - transform.position.y) / screenHeight) + 0.5f;
+		float speed = Mathf.Max( Mathf.Lerp( minSpeed, maxSpeed, speedFactor ), minSpeed );
+		Vector3 offset = Vector3.up * speed * Time.deltaTime;
+
+		transform.Translate( offset );
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager instance { get; private set; }
+
+	public List<GameObject> players;
+
+	void Awake()
+	{
+		if ( instance == null )
+		{
+			instance = this;
+			DontDestroyOnLoad( gameObject );
+		}
+		else if ( instance != this )
+		{
+			Destroy( gameObject );
+		}
+	}
+
+	void Start()
+	{
+		players = new List<GameObject>();
+	}
+}
