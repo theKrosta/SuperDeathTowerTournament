4ddb811d5a055c99855df316f9b2957b02c22293
Davide Comoretto <d.comoretto@tiscali.it>
2017-06-04 22:54:43 +0200

    Physics settings, player grounded check

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -1,21 +1,30 @@
 ﻿using UnityEngine;
 
-[RequireComponent( typeof( Rigidbody2D ) )]
+[RequireComponent( typeof( Collider2D ), typeof( Rigidbody2D ) )]
 public class PlayerMover : MonoBehaviour
 {
 	public float jumpingForce;
 	public float walkingSpeed;
+	
+	private int groundMask;
 
+	private Collider2D coll;
 	private Rigidbody2D rbody;
 
 	void Awake()
 	{
+		groundMask = LayerMask.GetMask( "Platform" );
+
+		coll = GetComponent<Collider2D>();
 		rbody = GetComponent<Rigidbody2D>();
 	}
 
 	public void Jump()
 	{
-		rbody.AddForce( Vector2.up * jumpingForce, ForceMode2D.Impulse );
+		if ( CheckIfGrounded() )
+		{
+			rbody.AddForce( Vector2.up * jumpingForce, ForceMode2D.Impulse );
+		}
 	}
 
 	public void Walk( float offset )
@@ -23,5 +32,11 @@ public class PlayerMover : MonoBehaviour
 		Vector2 velocity = rbody.velocity;
 		velocity.x = offset * walkingSpeed;
 		rbody.velocity = velocity;
+	}
+
+	private bool CheckIfGrounded()
+	{
+		RaycastHit2D ground = Physics2D.Raycast( transform.position, Vector3.down, Mathf.Infinity, groundMask );
+		return coll.IsTouching( ground.collider );
 	}
 }
